0ceba957c83d86dc25abf08add5397682e450fea
Aghavni Zatikyan <asya.zatikyan.24@gmail.com>
2023-02-11 00:31:38 +0300

    Add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿/*
+Задача 36: Задайте одномерный массив, заполненный случайными 
+числами. Найдите сумму элементов, стоящих на нечётных позициях.
+[3, 7, 23, 12] -> 19
+[-4, -6, 89, 6] -> 0
+*/
+int[] InitArray()
+{
+    int[] result = new int[4];
+    Random rnd = new Random();
+    for (int i = 0; i < result.Length; i++)
+    {
+        result[i] = rnd.Next(-100, 100);
+    }
+    return result;
+}
+
+
+
+void PrintArray (int []arr)
+{
+    Console.Write('[');
+    for (int i = 0; i < arr.Length; i++)
+    {
+       Console.Write($"{arr[i]} ");
+       if (i == arr.Length-1)
+       Console.Write(']'); 
+    }
+     Console.WriteLine();
+}
+
+
+int GetSummOfNegatives (int[]arr)
+{
+    int sumResult = 0;
+    for (int i = 1; i < arr.Length; i+=2)
+    {
+       sumResult = sumResult + arr[i];
+    } 
+    return sumResult;
+}
+
+
+
+
+int [] arr = InitArray();
+PrintArray (arr);
+int SummOfNegatives = GetSummOfNegatives (arr);
+Console.WriteLine ($"Cумму элементов, стоящих на нечётных позициях = {SummOfNegatives}"); 
